493ff0ecd9cc08e7b2f2771e0293aec09326dfa7
Rafael Felipe <allain.123ant@gmail.com>
2025-11-08 20:52:33 -0300

    Update lif.c
    só adicionei um tratamento de erro eu se fosse você perguntasse o chat gpt ou qualquer IA como fazer um code ai você já acostuma a colocar tratamento de erro

--- lif/src/lif.c
+++ lif/src/lif.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <dirent.h>
 
-
-void listfiles(char* path) {
-    DIR *d;
-    struct dirent *dir;
-    d = opendir(path); 
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            printf("%s\t", dir->d_name); 
-        }
-        closedir(d);
+int listfiles(char *path) {
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(path);
+    if (dir == NULL){
+        perror("não foi possivel abrir o arquivo");
+        return 1;
+    } // um tratamento de error não mata okay !
+    while ((entry = readdir(dir)) != NULL){
+        printf("%s\n", entry->d_name);
     }
+    closedir(dir);
+    return 1;
 }
-
 int main(int argc, char* argv[]) {
+    if (argc < 1){
+        printf("error: argumentos insuficientes");
+        return 1;
+    }
     listfiles(argv[1]);
+    return 0;
 }
